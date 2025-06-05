10015327b3139b7b2826ea868094806aa208ea49
Chamod Shyamal <129838930+madusanakcs@users.noreply.github.com>
2025-06-05 19:30:43 +0530

    Delete Desktop directory

--- Desktop/An/use.py
+++
@@ -1,9 +0,0 @@
-from main_predictor import MainPredictor
-
-img_path = "1.png"
-user_need = "both"  # "char", "era", or "both"
-
-predictor = MainPredictor(user_need=user_need)
-results = predictor.predict(img_path)
-
-# Now you can access results["character"], results["era"], etc.
